400160afa90ee17829b13ebc985d997b112ae1ab
jsaracho <joha.1995.saracho@gmail.com>
2022-03-05 10:20:37 -0300

    Cambios

---
+++ gespro/src/main/java/presentacion/RolView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentacion;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Model;
+
+/**
+ *
+ * @author user
+ */
+@Model
+@SessionScoped
+public class RolView implements Serializable{
+}
---
+++ gespro/src/main/java/presentacion/UserView.java
@@ -0,0 +1,15 @@
+package presentacion;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Model;
+
+/**
+ *
+ * @author joha
+ */
+@Model
+@SessionScoped
+public class UserView implements Serializable{
+    
+}
